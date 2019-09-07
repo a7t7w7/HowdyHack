01d6bbb4a9d08e17dda062c78d8d2a2cb824a5eb
ShatteredIce <icywind.katsuo@gmail.com>
2019-09-07 11:53:25 -0500

    simple data parser

--- Main.py
+++ Main.py
@@ -1,3 +1,21 @@
 # HowdyHack 2019
 
-print("Yeet x2")
+data_file = "201931.json"
+
+try:
+    f = open(data_file, "r")
+    f1 = f.readlines()
+    for line in f1:
+        if len(line) > 11 and line[11] == '{':
+            print("CRN:",line[3:8])
+        if len(line) > 8 and line[5:9] == "name":
+            print("Name:",line[13:-3])
+        if len(line) > 8 and line[5:10] == "title":
+            print("Title:",line[14:-3])
+    f.close()
+except FileNotFoundError:
+    print("Error: data file does not exist")
+
+
+
+
